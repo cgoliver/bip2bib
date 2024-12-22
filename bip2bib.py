097e085d7b344c51ea0efc114e13caa0324da69d
cgoliver <carlos.gonzalez.oliver@gmail.com>
2024-12-22 09:17:29 -0500

    dump verses

--- bip2bib.py
+++ bip2bib.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 import hashlib
@@ -11,7 +12,7 @@ def cline():
         "-s", "--secret", default="", type=str, help="Optional string appended to word index before hashing"
     )
     parser.add_argument(
-        "-o", "--outfile", default="bip_to_verse.py", type=str, help="Path to output file where mapping is stored."
+        "-o", "--outfile", default="mapping.txt", type=str, help="Path to output file where mapping is stored."
     )
     return parser.parse_args()
     pass
@@ -40,24 +41,31 @@ def load_bip39():
     return [word.strip() for word in open("english.txt").readlines()]
 
 
-def build_index(args) -> str:
+def dump_bible():
     verselist = []
-    all_books = bible.Book
-    for book in all_books:
-        num_chapters = bible.get_number_of_chapters(book)
-        for chapter in range(1, num_chapters + 1):
-            num_verses = bible.get_number_of_verses(book, chapter)
-            for verse in range(1, num_verses + 1):
-                verse_id = bible.get_verse_id(book, chapter, verse)
-                ref = bible.convert_verse_ids_to_references([verse_id])
-                name = bible.format_scripture_references(ref)
-                verselist.append(name)
+    with open("verses.txt", "w") as bib:
+        all_books = bible.Book
+        for book in all_books:
+            num_chapters = bible.get_number_of_chapters(book)
+            for chapter in range(1, num_chapters + 1):
+                num_verses = bible.get_number_of_verses(book, chapter)
+                for verse in range(1, num_verses + 1):
+                    verse_id = bible.get_verse_id(book, chapter, verse)
+                    ref = bible.convert_verse_ids_to_references([verse_id])
+                    name = bible.format_scripture_references(ref)
+                    bib.write(f"{name}\n")
 
+
+def build_index(args) -> str:
+    if not os.path.exists("verses.txt"):
+        print("Writing verse list to verses.txt")
+        dump_bible()
+    verses = [verse.strip() for verse in open("verses.txt", "r").readlines()]
     wordlist = load_bip39()
     with open(args.outfile, "w") as outf:
         for idx, word in enumerate(wordlist):
-            new_idx = idx_hash(idx, secret=args.secret, mod=len(verselist))
-            outf.write(f"{word} {verselist[new_idx]}\n")
+            new_idx = idx_hash(idx, secret=args.secret, mod=len(verses))
+            outf.write(f"{word} {verses[new_idx]}\n")
 
 
 if __name__ == "__main__":
